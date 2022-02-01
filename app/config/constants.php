42c062ded800333b7ac9136df4e6f7aace5c088a
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2022-02-01 21:08:35 +0700

    Add web directory constant

--- app/config/constants.php
+++ app/config/constants.php
@@ -85,5 +85,6 @@ defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automat
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
 
-//APP VERSION
+//APP CONFIG
 defined('APP_VERSION')    	   OR define('APP_VERSION', '1.0.1');
+defined('WEBDIR')			   OR define('WEBDIR', '');
